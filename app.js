be8a79cd2022bfe0cb87a206826944ebb1519840
dmquinn <daniel.murphy.berlin@gmail.com>
2021-02-03 11:07:30 +0100

    updated showcase site,  basic express app

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("connected");
+
+const express = require("express");
+const ejsMate = require("ejs-mate");
+const zoom = require("js-image-zoom");
+const secret = process.env.SECRET || "thisworks";
+const path = require("path");
+const app = express();
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`serving on port ${port}`);
+});
